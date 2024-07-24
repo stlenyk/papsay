ad12f479d9219f62b6dfba9a4b3c3e193bdd3e14
stlenyk <stlenyk@gmail.com>
2024-07-24 04:50:02 +0200

    Add more papieże

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,46 @@ use rand::Rng;
 use rand_distr::Distribution;
 use unicode_segmentation::UnicodeSegmentation;
 
-#[derive(Parser)]
-struct Args {
-    message: Option<String>,
+#[derive(Clone)]
+enum Papież {
+    Ascii,
+    UTF8,
+    FromFile(String),
 }
 
-fn pappify(s: &str) -> String {
-    let message = s.replace('\t', "    ");
+impl std::str::FromStr for Papież {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "ascii" => Ok(Papież::Ascii),
+            "utf8" => Ok(Papież::UTF8),
+            s => Ok(Papież::FromFile(s.to_owned())),
+        }
+    }
+}
+
+impl Papież {
+    fn text(&self) -> String {
+        match self {
+            Papież::Ascii => include_str!("../papieże/ascii.pap").to_owned(),
+            Papież::UTF8 => include_str!("../papieże/utf8.pap").to_owned(),
+            Papież::FromFile(path) => std::fs::read_to_string(path).unwrap(),
+        }
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    message: Option<String>,
+    #[arg(short, long, default_value = "utf8")]
+    /// One of: ascii, utf8, or a path to a file containing a custom papież.
+    papież: Papież,
+}
+
+fn pappify(message: &str, papież: &Papież) -> String {
+    let message = message.replace('\t', "    ");
 
     let message_lines = textwrap::wrap(&message, 40);
     let lines_graphemes: Vec<Vec<_>> = message_lines
@@ -54,26 +87,26 @@ fn pappify(s: &str) -> String {
     let top_border = format!(" {} ", "_".repeat(n_cols + 2));
     let bot_border = format!(" {} ", "-".repeat(n_cols + 2));
     let message_text = format!("{}\n{}\n{}", top_border, message_text, bot_border);
-    const PAP_TEXT: &str = include_str!("../papjesz.pap");
 
-    format!("{}\n{}", message_text, PAP_TEXT)
+    format!("{}\n{}", message_text, papież.text())
 }
 
 fn main() {
     let args = Args::parse();
     let message = args.message;
 
-    if !atty::is(Stream::Stdin) {
+    let message = if !atty::is(Stream::Stdin) {
         let mut message = Vec::new();
         io::stdin().read_to_end(&mut message).unwrap();
         let message = String::from_utf8(message).unwrap();
         let message = message.trim_end();
-        println!("{}", pappify(message));
+        message.to_owned()
     } else if let Some(message) = message {
-        println!("{}", pappify(&message));
+        message
     } else {
         let lines_database = include_str!("../transkrypcja-bez-didaskaliów.txt")
             .lines()
+            .filter(|s| !s.is_empty())
             .collect::<Vec<_>>();
 
         let distribution = rand_distr::Normal::<f32>::new(3.0, 1.0).unwrap();
@@ -81,7 +114,8 @@ fn main() {
         let n_lines = distribution.sample(&mut rng).round() as usize;
         let idx = rng.gen_range(0..lines_database.len());
 
-        let message = lines_database[idx..(idx + n_lines).min(lines_database.len())].join("\n");
-        println!("{}", pappify(&message));
-    }
+        lines_database[idx..(idx + n_lines).min(lines_database.len())].join("\n")
+    };
+
+    println!("{}", pappify(&message, &args.papież));
 }
